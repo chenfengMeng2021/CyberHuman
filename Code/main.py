86855edbe7ca515ae362ef5bf84b315407e1777f
chenfeng meng <chenfengmeng0@gmail.com>
2023-08-16 15:21:24 -0400

    better GUI

--- Code/main.py
+++ Code/main.py
@@ -11,6 +11,7 @@ import threading
 from getVoice import AudioRecorder
 from PIL import Image, ImageTk
 from Text2Image import ImageGenerator
+import os
 
 positive_prompt = "1 girl, solo, smiling, light blue eyes, sugar pink hair, white beanie, pink cloth, white mini skirt, knee length socks, hearts, cute, sparkly, holding cat, chibi, (pastel pink), glitter"
 negative_prompt = "NG_DeepNegative_V1_75T, EasyNegativeV2,  extra fingers, fewer fingers, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, (worst quality, low quality:1.4), Negative2, (low quality, worst quality:1.4), (bad anatomy), (inaccurate limb:1.2), bad composition, inaccurate eyes, extra digit,fewer digits, (extra arms:1.2), (bad-artist:0.6), bad-image-v2-39000,"
@@ -46,6 +47,10 @@ def initial():
     input_win = tk.Tk()
     input_win.title("Initial Setting")
 
+    file_path = "../prompt/characters"
+    characters = os.listdir(file_path)
+    print(characters)
+
 
     def on_language_change(event):
         language = language_var.get()
@@ -68,6 +73,7 @@ def initial():
             response = "Hi, Nice to meet you"
         speak_out(response, name_var.get())
 
+
     def submit():
         nonlocal openai_key
         nonlocal voice
@@ -75,47 +81,76 @@ def initial():
         voice = name_var.get()
         input_win.destroy()
 
-    lbl_prompt = tk.Label(input_win, text="Enter your Openai API key:")
-    lbl_prompt.grid(row=0, column=0, padx=10, pady=10)
+    lbl_label = tk.Label(input_win, text="Enter your Openai API key:")
+    lbl_label.grid(row=0, column=1, padx=10, pady=10)
 
     key_input = tk.Entry(input_win)
-    key_input.grid(row=0, column=1, padx=10, pady=10)
+    key_input.grid(row=0, column=2, padx=10, pady=10)
 
-    language_prompt = tk.Label(input_win, text="Language")
-    language_prompt.grid(row=1, column=0, padx=10, pady=10)
+    # Sound bar
+    voice_label = tk.Label(input_win, text="Voice: ")
+    voice_label.grid(row=1, column=0, padx=10, pady=10)
+
+    language_label = tk.Label(input_win, text="Language")
+    language_label.grid(row=1, column=1, padx=10, pady=10)
 
     language_var = tk.StringVar()
     language_dropdown = ttk.Combobox(input_win, textvariable=language_var, values=df['Language'].unique().tolist())
     language_dropdown.bind('<<ComboboxSelected>>', on_language_change)
-    language_dropdown.grid(row=1, column=1, padx=10, pady=10)
+    language_dropdown.grid(row=1, column=2, padx=10, pady=10)
 
-    gender_prompt = tk.Label(input_win, text="Gender")
-    gender_prompt.grid(row=2, column=0, padx=10, pady=10)
+    gender_label = tk.Label(input_win, text="Gender")
+    gender_label.grid(row=2, column=1, padx=10, pady=10)
 
     gender_var = tk.StringVar()
     gender_dropdown = ttk.Combobox(input_win, textvariable=gender_var)
     gender_dropdown.bind('<<ComboboxSelected>>', on_gender_change)
-    gender_dropdown.grid(row=2, column=1, padx=10, pady=10)
+    gender_dropdown.grid(row=2, column=2, padx=10, pady=10)
 
-    voice_prompt = tk.Label(input_win, text="Voice")
-    voice_prompt.grid(row=3, column=0, padx=10, pady=10)
+    timbre_label = tk.Label(input_win, text="Timbre")
+    timbre_label.grid(row=3, column=1, padx=10, pady=10)
 
     name_var = tk.StringVar()
     name_dropdown = ttk.Combobox(input_win, textvariable=name_var)
     name_dropdown.bind('<<ComboboxSelected>>', on_voice_chosen)
-    name_dropdown.grid(row=3, column=1, padx=10, pady=10)
+    name_dropdown.grid(row=3, column=2, padx=10, pady=10)
+
+    #Prompt dropdown
+    prompt_label = tk.Label(input_win, text="Prompt setting")
+    prompt_label.grid(row=4, column=0, padx=10, pady=10)
+
+    character_label = tk.Label(input_win, text="Character Selection")
+    character_label.grid(row=4, column=1, padx=10, pady=10)
+
+    character_var = tk.StringVar()
+    character_dropdown = ttk.Combobox(input_win, textvariable=character_var)
+    character_dropdown['values'] = characters
+    character_dropdown.grid(row=4, column=2, padx=10, pady=10)
+
+    #Picture dropdown
+    picture_label = tk.Label(input_win, text="Picture setting ")
+    picture_label.grid(row=5, column=0, padx=10, pady=10)
+
+    style_label = tk.Label(input_win, text="style selection: ")
+    style_label.grid(row=5, column=1, padx=10, pady=10 )
+
+    style_var = tk.StringVar()
+    style_dropdown = ttk.Combobox(input_win, textvariable=style_var)
+    style_dropdown['values'] = ["animation", "realistic", "photographic", "2.5D"]
+    style_dropdown.grid(row=5, column=2, padx=10, pady=10)
 
     submit_button = ttk.Button(input_win, text="Submit", command=submit)
-    submit_button.grid(row=4, column=0, padx=10, pady=10, columnspan=3)
+    submit_button.grid(row=10, column=1, padx=10, pady=10, columnspan=3)
 
     input_win.mainloop()
-
-    return voice, openai_key
+    character_path = f'../prompt/characters/{character_var.get()}'
+    print(style_var.get())
+    return voice, openai_key, character_path, style_var.get()
 
 
 
 def main():
-    voice, api_key = initial()
+    voice, api_key, character_path, style = initial()
     recorder = AudioRecorder()
     chatmodel = ConversationBot(api_key, voice)
     model = ImageGenerator(ckptmodeldict["cute_animation"])
@@ -153,7 +188,6 @@ def main():
             user_speech, assistant_response = chatmodel.conversation()
             lbl.config(text="drawing")
 
-
             #generate picture
             image = model.generate(style, positive_prompt, negative_prompt, loras, width//2, height//2)
 
@@ -179,8 +213,6 @@ def main():
 
             #set the lbl again
             lbl.config(text = "Press the spacebar to start recording. ")
-
-
 
 
     root = tk.Tk()
